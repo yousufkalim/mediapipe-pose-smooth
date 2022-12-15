f4a60e6fd44518801bdf15165711d2253afd2714
Yousuf Kalim <yousufkalim@outlook.com>
2022-12-15 10:01:50 +0500

    Smooth landmarks function created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
-  return returnStr;
+/**
+ * This will remove the jitter and smooth the landmarks given by Mediapipe
+ * @author Yousuf Kalim
+ */
+
+let frameSets = [];
+let smoothFrame = [];
+
+/**
+ * smoothLandmarks
+ * @param {Object} results This should be coming directly from Mediapipe
+ * @param {Function} onResults Optional: If you want to call other function instead of getting return
+ * @returns {Void}
+ */
+const smoothLandmarks = (results, onResults) => {
+  // Pushing frame at the end of frameSet array
+  if (results.poseLandmarks) {
+    frameSets.push(results.poseLandmarks);
+  }
+
+  if (frameSets.length === 8) {
+    // This loop will run 33 time to make average of each joint
+    for (let i = 0; i < 33; i++) {
+      // Making an array of each joint coordinates
+      let x = frameSets.map((a) => a[i].x);
+      let y = frameSets.map((a) => a[i].y);
+      let z = frameSets.map((a) => a[i].z);
+      let visibility = frameSets.map((a) => a[i].visibility);
+
+      // Sorting the array into ascending order
+      x = x.sort((a, b) => a - b);
+      y = y.sort((a, b) => a - b);
+      z = z.sort((a, b) => a - b);
+      visibility = visibility.sort((a, b) => a - b);
+
+      // Dropping 2 min and 2 max coordinates
+      x = x.slice(2, 6);
+      y = y.slice(2, 6);
+      z = z.slice(2, 6);
+      visibility = visibility.slice(2, 6);
+
+      // Making the average of 4 remaining coordinates
+      smoothFrame[i] = {
+        x: x.reduce((a, b) => a + b, 0) / x.length,
+        y: y.reduce((a, b) => a + b, 0) / y.length,
+        z: z.reduce((a, b) => a + b, 0) / z.length,
+        visibility: visibility.reduce((a, b) => a + b, 0) / visibility.length,
+      };
+    }
+
+    // Removing the first frame from frameSet
+    frameSets.shift();
+  }
+
+  // after first 8 frames we have averaged coordinates, So now updating the poseLandmarks with averaged coordinates
+  if (smoothFrame.length > 0) {
+    results.poseLandmarks = smoothFrame;
+  }
+
+  return onResults ? onResults(results) : results;
 };
 
-const awesomeFunction = () => 'I am just an Awesome Function';
-
-export default defaultAwesomeFunction;
-
-export { awesomeFunction };
+export default smoothLandmarks;
